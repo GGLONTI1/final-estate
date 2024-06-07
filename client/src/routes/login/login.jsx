fc720494a96909c2aed5e5b9a8fc67da9960e492
GGLONTI1 <giorgiglonti23@gmail.com>
2024-06-08 00:34:39 +0400

    LoginCreated

--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -1,15 +1,47 @@
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./login.scss";
-import { Link } from "react-router-dom";
 
 function Login() {
+  const [error, setError] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setIsLoading(true)
+    const { username, password } = Object.fromEntries(new FormData(e.target))
+
+    if (!username || !password) {
+      setError("All fields are required!")
+      return
+    }
+    try {
+      const res = await axios.post("http://localhost:5005/api/auth/login", {
+        username,
+        password,
+      })
+      setError(res.data.message)
+      setTimeout(() => {
+        navigate("/")
+      }, 2000)
+    } catch (error) {
+      setError(error.response.data.message)
+    } finally {
+      setIsLoading(false)
+    }
+  }
   return (
     <div className="login">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Welcome back</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="password" type="password" placeholder="Password" />
-          <button>Login</button>
+          <button disabled={isLoading}>
+            {isLoading ? "Loading..." : "Login"}
+          </button>
           <Link to="/register">{"Don't"} you have an account?</Link>
         </form>
       </div>
--- client/src/routes/register/register.jsx
+++ client/src/routes/register/register.jsx
@@ -1,7 +1,7 @@
-import { Link, useNavigate } from "react-router-dom";
 import axios from "axios"
-import "./register.scss";
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./register.scss";
 
 
 function Register() {
